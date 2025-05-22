3c76fc600e53d053fd738cfa2c311044f8fff7ce
Jose Risomar Sousa <jrsousa2@hotmail.com>
2025-05-22 20:15:50 -0300

    Updt

---
+++ setup.py
@@ -0,0 +1,18 @@
+from cx_Freeze import setup, Executable
+ 
+# Paths to include
+include_files = ["D:\\Python\\iTunes" , "D:\\Python\\WMP", "D:\\Python\\Plays_Sync"]
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_options = {'packages': ["win32com.client"], 'excludes': ["sqlite3"], "include_msvcr": True}
+ 
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+ 
+executables = [Executable("D:\\Python\\Plays_Sync\Main.py", base="Win32GUI", target_name = "Main.exe")]
+ 
+setup(name="The Plays Sync Tool",
+      version = "1.0",
+      description = "Sync WMP and iTunes Plays",
+      options = {'build_exe': build_options},
+      executables = executables)
